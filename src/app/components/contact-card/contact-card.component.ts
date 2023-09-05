0bf7886be5b884f1b90dc236cf4b4e16ffc519a9
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-09-05 04:04:33 -0500

    Implementar componente card contact
    Se implementó el componente de carta de contacto para mostrar la información de los desarrolladores en la página de contacto.

--- src/app/components/contact-card/contact-card.component.ts
+++ src/app/components/contact-card/contact-card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {  } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-card',
@@ -7,39 +8,9 @@ import {  } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./contact-card.component.css']
 })
 export class ContactCardComponent {
-  developers = [
-    {
-      name: 'Ronny García Zambrano',
-      description: 'Desarrollador FullStack con experiencia en la creación de soluciones escalables y eficientes.',
-      socialLinks: [
-        { platform: 'LinkedIn', url: 'enlace-linkedin-desarrollador-1' },
-        { platform: 'GitHub', url: 'enlace-twitter-desarrollador-1' }
-      ]
-    },
-    {
-      name: 'Desarrollador 2',
-      description: 'Desarrollador backend ',
-      socialLinks: [
-        { platform: 'LinkedIn', url: 'enlace-linkedin-desarrollador-2' },
-        { platform: 'GitHub', url: 'enlace-github-desarrollador-2' }
-      ]
-    }
-  ];
+  @Input() developer: any;
 
-  restaurantProjectDescription = `
-    Nuestra WebApp de Restaurante es la solución perfecta para amantes de la comida y la comodidad.
-    Te ofrecemos una experiencia culinaria única desde la comodidad de tu hogar. Explora nuestro
-    variado menú de platos deliciosos y realiza pedidos en línea para entrega a domicilio o para llevar.
-
-    Características de la WebApp de Restaurante:
-    - Menú en línea con una amplia gama de opciones culinarias.
-    - Realiza pedidos en línea y personaliza tus platos según tus preferencias.
-    - Opciones de entrega a domicilio o para llevar, según tus necesidades.
-    - Fácil seguimiento del estado de tus pedidos en tiempo real.
-    - Reserva de mesas en nuestro acogedor restaurante directamente desde la web.
-
-    Nos enorgullece brindarte una experiencia gastronómica excepcional y un servicio al cliente de primer nivel.
-    ¡Esperamos verte pronto y satisfacer tus antojos culinarios!
-  `;
-
+  protected faGithub = faGithub;
+  protected faLinkedin = faLinkedin;
+  protected faUser = faUser;
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  protected developers = [
+    {
+      name: 'Ronny García Zambrano',
+      user: 'rsgarcia0203',
+      description: 'Desarrollador FullStack con experiencia en C#, PHP, Python, Java, JavaScript, Dart, .Net, Xamarin, Laravel, Flask, Spring, Angular, Express, Nest.js, Next.js y Flutter.  Siempre buscando la creación de soluciones escalables y eficientes.',
+      linkedin: 'https://www.linkedin.com/in/rsgarcia0203/',
+      github: 'https://github.com/rsgarcia0203',
+      photo: 'https://media.licdn.com/dms/image/C5603AQG_T_soyNlyHg/profile-displayphoto-shrink_200_200/0/1628449553784?e=1699488000&v=beta&t=b-2HJvCk9nZVy7OZ09_FHvIvQQK0I0IoH6XANxolh7w'
+    },
+    {
+      name: 'Keneth Pacheco Suarez',
+      user: 'kenethPs',
+      description: 'Desarrollador Back-End con experiencia en Python, Node.js y Express.  Siempre buscando conseguir el AP!!',
+      linkedin: 'https://www.linkedin.com/in/keneth-pacheco-su%C3%A1rez-2062681a3/',
+      github: 'https://github.com/kenethPs',
+      photo: 'https://scontent.fgye1-1.fna.fbcdn.net/v/t39.30808-6/212462964_4184301238316076_3162189359004692810_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=1b51e3&_nc_eui2=AeFMmZ8GCkNXj73xHT-VySZUkKZ5_J8eSCeQpnn8nx5IJ53m46CCvrQT7v7aE53W5MZF5Buk41rj9Nz1_zoW2Mdz&_nc_ohc=vU6jLlcqhgMAX97bQG_&_nc_ht=scontent.fgye1-1.fna&oh=00_AfCzCecLqKVj37CvwNLRBptYFUw3uutct63z4GgAAMHRgg&oe=64FBCA04'
+    }
+  ];
 }
